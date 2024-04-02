6fe6640446b15afccfbe5ede9303c40943f6e405
amineyesilyurt <amine.yesilyurt@gmail.com>
2024-04-02 15:04:18 +0300

    dev 2

--- lala.java
+++ lala.java
@@ -1,6 +1,6 @@
-String commit1= "local";
+String commit1= "localv2";
 String commit2=";
 String commit3=";
-String commit4="developmentlocal";
+String commit4="developmentlocalv2";
 String commit5="";
 String commit6
