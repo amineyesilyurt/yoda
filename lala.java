e88d1e361dcf9cafb545a8f4f1922b2edeea8a01
amineyesilyurt <amine.yesilyurt@gmail.com>
2024-04-02 17:37:58 +0300

    master 7

--- lala.java
+++ lala.java
@@ -1,6 +1,6 @@
-String commit1= "master 6";
+String commit1= "master 7";
 String commit2=";
 String commit3=";
-String commit4="developer 6";
+String commit4="master 7";
 String commit5="";
 String commit6
