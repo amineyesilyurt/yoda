1c5cfc706b671ebccfd018d5448489378b265e89
amineyesilyurt <amine.yesilyurt@gmail.com>
2024-04-02 17:52:55 +0300

    features done

--- lala.java
+++ lala.java
@@ -1,7 +1,7 @@
 String commit1= "dev 7";
 String commit2=";
 String commit3=";
-String commit4="developer 7";
+String commit4="developer feaures added";
 String commit5="";
 String commit6
 dev feature 1
