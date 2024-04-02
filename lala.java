33084a6ad7ad61ebcb86ca1a01824c2a6b1fdc73
amineyesilyurt <amine.yesilyurt@gmail.com>
2024-04-02 15:29:26 +0300

    mas v4

--- lala.java
+++ lala.java
@@ -1,6 +1,6 @@
-String commit1= "masterv1";
+String commit1= "mas4";
 String commit2=";
 String commit3=";
-String commit4="developmentlocalv2";
+String commit4="mas4";
 String commit5="";
 String commit6
