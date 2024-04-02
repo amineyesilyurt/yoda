558df0791907ec99e605c9d06e59b6d0257e469c
amineyesilyurt <amine.yesilyurt@gmail.com>
2024-04-02 15:43:26 +0300

    developer v5

--- lala.java
+++ lala.java
@@ -1,7 +1,7 @@
 
-String commit1= "dev4";
+String commit1= "developer 5";
 String commit2=";
 String commit3=";
-String commit4="dev4";
+String commit4="developer 5";
 String commit5="";
 String commit6
